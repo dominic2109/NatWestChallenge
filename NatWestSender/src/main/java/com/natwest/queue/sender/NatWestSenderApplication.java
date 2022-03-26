4d681dc688b65603a6e3a9b559ccb4731b2d0289
dominic2109 <87693632+dominic2109@users.noreply.github.com>
2022-03-26 17:34:54 +0530

    Project setup, initial commit

---
+++ NatWestSender/src/main/java/com/natwest/queue/sender/NatWestSenderApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.queue.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatWestSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatWestSenderApplication.class, args);
+	}
+
+}
---
+++ NatWestSender/src/test/java/com/natwest/queue/sender/NatWestSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.queue.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatWestSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
